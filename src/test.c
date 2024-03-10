cdf69b0cbdf38696f3abd42677e6eecd7898b3b3
Daniel <dakj@itu.dk>
2024-03-10 15:37:02 +0100

    Test setup to use yaml
    Co-authored-by: Jeppe Lindhard <jepli@itu.dk>
    Co-authored-by: Nikolaj Sørensen <nikso@itu.dk>

--- src/test.c
+++ src/test.c
@@ -3,9 +3,12 @@
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#include "utils/yaml.c"
+#include <sys/ipc.h>
 
 #define FILENAME_INPUT "input.png"
 #define FILENAME_OUTPUT "output.png"
+#define FILENAME_CONFIG "config.yaml"
 
 void save_image(const char *filename, const ImageBatch *batch)
 {
@@ -29,31 +32,16 @@ void load_image(const char *filename, ImageBatch *batch)
     batch->width = image_width;
     batch->channels = image_channels;
     batch->num_images = 1;
+    batch->shm_key = 1;
     size_t image_size = image_height * image_width * image_channels;
-    batch->data = (unsigned char*)malloc(image_size);
-    memcpy(batch->data, image_data, image_size);
-}
-
-ModuleConfig* module_configurations()
-{
-    ModuleConfig flip_config = MODULE_CONFIG__INIT;
-    ConfigParameter flip_percent = CONFIG_PARAMETER__INIT;
-    ConfigParameter flip_amount = CONFIG_PARAMETER__INIT;
-    flip_percent.key = "flip_percent";
-    flip_percent.value_case = CONFIG_PARAMETER__VALUE_FLOAT_VALUE;
-    flip_percent.float_value = 0.20;
-    flip_amount.key = "flip_amount";
-    flip_amount.value_case = CONFIG_PARAMETER__VALUE_INT_VALUE;
-    flip_amount.int_value = 1;
-    flip_config.parameters = malloc(sizeof(ConfigParameter *) * 2);
-    flip_config.n_parameters = 2;
-    flip_config.parameters[0] = &flip_percent;
-    flip_config.parameters[1] = &flip_amount;
-    size_t lenConfig = module_config__get_packed_size(&flip_config);
-    uint8_t bufConfig[lenConfig];
-    module_config__pack(&flip_config, bufConfig);
-    ModuleConfig *unpacked_config = module_config__unpack(NULL, lenConfig, bufConfig);
-    return unpacked_config;
+    if (SHARED_MEMORY) {
+        int shmid = shmget(batch->shm_key, image_size, IPC_CREAT | 0666);
+        char *shmaddr = shmat(shmid, NULL, 0);
+        memcpy(shmaddr, image_data, image_size);
+    } else {
+        batch->data = (unsigned char*)malloc(image_size);
+        memcpy(batch->data, image_data, image_size);
+    }
 }
 
 int main()
@@ -61,12 +49,13 @@ int main()
     ImageBatch batch;
     load_image(FILENAME_INPUT, &batch);
 
-    ModuleConfig* config = module_configurations();
+	ModuleConfig config = MODULE_CONFIG__INIT;
+	if (parse_module_yaml_file(FILENAME_CONFIG, &config) < 0)
+		return -1;
     
-    ImageBatch result = run(&batch, config);
+    ImageBatch result = run(&batch, &config);
 
     save_image(FILENAME_OUTPUT, &result);
-    free(config->parameters);
-    free(result.data);
+    free(config.parameters);
     return 0;
 }
---
+++ src/utils/yaml.c
@@ -0,0 +1,227 @@
+#include <yaml.h>
+#include <errno.h>
+#include <math.h>
+#include "module_config.pb-c.h"
+
+int initialize_parser(const char *filename, yaml_parser_t *parser, FILE *fh)
+{
+    fh = fopen(filename, "r");
+
+    /* Initialize parser */
+    if (!yaml_parser_initialize(parser))
+    {
+        fprintf(stderr, "Error: Failed to initialize parser!\n");
+        return -1;
+    }
+    if (fh == NULL)
+    {
+        fprintf(stderr, "Error: Failed to open file!\n");
+        return -1;
+    }
+
+    /* Set input file */
+    yaml_parser_set_input_file(parser, fh);
+
+    return 0;
+}
+
+void cleanup_resources(yaml_parser_t *parser, yaml_event_t *event, FILE *fh)
+{
+    if (event != NULL)
+    {
+        yaml_event_delete(event);
+    }
+    yaml_parser_delete(parser);
+
+    if (fh != NULL)
+    {
+        fclose(fh);
+    }
+}
+
+int safe_atof(const char *in, float *out)
+{
+    errno = 0; // To detect overflow or underflow
+    char *endptr;
+    float val = strtof(in, &endptr);
+
+    if (endptr == in)
+    {
+        fprintf(stderr, "Error: Value \"%s\" could not be parsed to a floating-point number.\n", in);
+        return -1;
+    }
+    else if (*endptr != '\0')
+    {
+        fprintf(stderr, "Error: Extra characters after number: \"%s\"\n", endptr);
+        return -1;
+    }
+    else if ((val == HUGE_VALF || val == -HUGE_VALF) && errno == ERANGE)
+    {
+        // Note: Checking against HUGE_VALF for float-specific overflow/underflow
+        fprintf(stderr, "Error: Value \"%s\" is out of range.\n", in);
+        return -1;
+    }
+
+    *out = val;
+    return 0;
+}
+
+int safe_atoi(const char *in, int *out)
+{
+    errno = 0; // To detect overflow
+    char *endptr;
+    long val = strtol(in, &endptr, 10); // Base 10 for decimal conversion
+
+    if (endptr == in)
+    {
+        fprintf(stderr, "Error: No digits were found in token %s\n", in);
+        return -1;
+    }
+    if (*endptr != '\0')
+    {
+        fprintf(stderr, "Error: Extra characters after number: %s in token %s\n", endptr, in);
+        return -1;
+    }
+    if ((val == LONG_MIN || val == LONG_MAX) && errno == ERANGE)
+    {
+        fprintf(stderr, "Error: Value out of long int range for token %s\n", in);
+        return -1;
+    }
+    if (val < INT_MIN || val > INT_MAX)
+    {
+        fprintf(stderr, "Error: Value out of int range for token %s\n", in);
+        return -1;
+    }
+
+    *out = (int)val;
+    return 0;
+}
+
+int parse_module_yaml_file(const char *filename, ModuleConfig *module_config)
+{
+    yaml_parser_t parser;
+    FILE *fh = NULL;
+    if (initialize_parser(filename, &parser, fh) < 0)
+        return -1;
+
+    /* Module definitions */
+    ConfigParameter *params = NULL;
+    int param_idx = -1;  // current module index
+    int param_count = 0; // find total amount of modules
+
+    /* START parsing */
+    yaml_event_t event;
+    while (1)
+    {
+        if (!yaml_parser_parse(&parser, &event))
+        {
+            printf("Error: Parser error %d\n", parser.error);
+            return -1;
+        }
+        switch (event.type)
+        {
+            case YAML_MAPPING_START_EVENT:
+                // New Dash
+                param_idx++;
+                ConfigParameter *temp = realloc(params, (param_count + 1) * sizeof(ConfigParameter));
+                if (!temp)
+                {
+                    fprintf(stderr, "Error: Failed to allocate memory for ConfigParameter during parsing\n");
+                    return -1;
+                }
+                params = temp;
+
+                // Fill in the new struct
+                ConfigParameter param = CONFIG_PARAMETER__INIT;
+                params[param_idx] = param;
+
+                param_count++;
+                break;
+            case YAML_SCALAR_EVENT:
+                // New set of ModuleDefinition encountered
+                if (strcmp((char *)event.data.scalar.value, "key") == 0)
+                {
+                    // Expect the next event to be the value of order
+                    if (!yaml_parser_parse(&parser, &event))
+                        break;
+                    params[param_idx].key = strdup((char *)event.data.scalar.value);
+                }
+                else if (strcmp((char *)event.data.scalar.value, "type") == 0)
+                {
+                    // Expect the next event to be the value of name
+                    if (!yaml_parser_parse(&parser, &event))
+                        break;
+                    if (safe_atoi((char *)event.data.scalar.value, &params[param_idx].value_case) < 0)
+                    {
+                        return -1;
+                    }
+                }
+                else if (strcmp((char *)event.data.scalar.value, "value") == 0)
+                {
+                    // Expect the next event to be the value of param_id
+                    if (!yaml_parser_parse(&parser, &event))
+                        break;
+                    switch (params[param_idx].value_case)
+                    {
+                        case CONFIG_PARAMETER__VALUE_BOOL_VALUE:
+                            if (strcmp((char *)event.data.scalar.value, "true") == 0)
+                            {
+                                params[param_idx].bool_value = 1;
+                            }
+                            else if (strcmp((char *)event.data.scalar.value, "false") == 0)
+                            {
+                                params[param_idx].bool_value = 0;
+                            }
+                            else
+                            {
+                                fprintf(stderr, "Error: Could not parse %s to boolean value\nAllowed values are: true, false\n", (char *)event.data.scalar.value);
+                                return -1;
+                            }
+                            break;
+                        case CONFIG_PARAMETER__VALUE_INT_VALUE:
+                            if (safe_atoi((char *)event.data.scalar.value, &params[param_idx].int_value) < 0)
+                            {
+                                return -1;
+                            }
+                            break;
+                        case CONFIG_PARAMETER__VALUE_FLOAT_VALUE:
+                            if (safe_atof((char *)event.data.scalar.value, &params[param_idx].float_value) < 0)
+                            {
+                                return -1;
+                            }
+                            break;
+                        case CONFIG_PARAMETER__VALUE_STRING_VALUE:
+                            params[param_idx].string_value = strdup((char *)event.data.scalar.value);
+                            break;
+                        default:
+                            fprintf(stderr, "Error: Value case %d unknown.\n", params[param_idx].value_case);
+                            return -1;
+                    }
+                }
+                else
+                {
+                    // Unexpected event type
+                    printf("Error: Unexpected YAML scalar value format: %s\nAllowed values are: key, type, value\n", (char *)event.data.scalar.value);
+                    return -1;
+                }
+            default:
+                break;
+        }
+        if (event.type == YAML_SEQUENCE_END_EVENT || event.type == YAML_DOCUMENT_END_EVENT)
+            break;
+
+        yaml_event_delete(&event);
+    }
+
+    // Insert ConfigParameter into ModuleConfig
+    module_config->n_parameters = param_count;
+    module_config->parameters = malloc(sizeof(ConfigParameter *) * param_count);
+    for (size_t i = 0; i < param_count; i++)
+    {
+        module_config->parameters[i] = &params[i];
+    }
+
+    cleanup_resources(&parser, &event, fh);
+
+    return 0;
+}
