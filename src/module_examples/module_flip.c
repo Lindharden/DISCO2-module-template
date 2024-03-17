2b007f4bdf271fc6c10e1560d0e83a0510cf4c24
Daniel <dakj@itu.dk>
2024-03-17 09:41:15 +0100

    Freshly updated modules
    Co-authored-by: Nikolaj Sørensen <nikso@itu.dk>
    Co-authored-by: Jeppe Lindhard <jepli@itu.dk>

--- src/module_examples/module_flip.c
+++ src/module_examples/module_flip.c
@@ -1,54 +1,68 @@
 #include "module.h"
 #include "util.h"
 
-ImageBatch run(ImageBatch *input_batch, ModuleParameterList *config) {
-    ImageBatch mirrored_batch;
-    mirrored_batch.width = input_batch->width;
-    mirrored_batch.height = input_batch->height;
-    mirrored_batch.channels = input_batch->channels;
-    mirrored_batch.num_images = input_batch->num_images;
+void module()
+{
+    int input_width = get_input_width();
+    int input_height = get_input_height();
+    int input_channels = get_input_channels();
 
-    float flip_percent = get_param_float(config, "flip_percent");
-    int flip_height = (int)(input_batch->height * flip_percent);
+    set_result_dimensions(input_width, input_height, input_channels);
+    set_result_num_images(get_input_num_images());
 
-    // Calculate the size of the entire batch
-    int data_size = input_batch->width * input_batch->height * input_batch->channels * input_batch->num_images;   
-    unsigned char *new_data = (unsigned char *)malloc(data_size);
+    float flip_percent = get_param_float("flip_percent");
+    int flip_height = (int)(get_input_height() * flip_percent);
 
-    if (new_data == NULL)
+    for (int i = 0; i < get_input_num_images(); ++i)
     {
-        fprintf(stderr, "[module_flip] Error: Unable to allocate memory.\n");
-        exit(EXIT_FAILURE);
-    }
+        unsigned char *input_image_data;
+        size_t img_size = get_image_data(i, &input_image_data);
+        unsigned char *output_image_data = (unsigned char *)malloc(img_size);
+        if (output_image_data == NULL)
+        {
+            fprintf(stderr, "[module_flip] Error: Unable to allocate memory.\n");
+            exit(EXIT_FAILURE);
+        }
 
-    mirrored_batch.data = new_data;
-    
-    // Calculate the size of each image in bytes
-    size_t image_size = input_batch->height * input_batch->width * input_batch->channels;
-
-    for (int i = 0; i < input_batch->num_images; ++i) {
-        unsigned char *image_data = &input_batch->data[i * image_size];
-        for (int y = 0; y < input_batch->height; ++y) {
-            for (int x = 0; x < input_batch->width / 2; ++x) {
+        for (int y = 0; y < input_height; ++y)
+        {
+            for (int x = 0; x < input_width / 2; ++x)
+            {
                 // Swap pixel values horizontally
-                for (int c = 0; c < input_batch->channels; ++c) {
-                    if (y > flip_height) {
-                        new_data[((y * input_batch->width + x) * input_batch->channels + c) + i * image_size] = 
-                            image_data[(y * input_batch->width + x) * input_batch->channels + c];
-                        new_data[((y * input_batch->width + (input_batch->width - 1 - x)) * input_batch->channels + c) + i * image_size] =
-                            image_data[(y * input_batch->width + (input_batch->width - 1 - x)) * input_batch->channels + c];
-                    } else {
-                        unsigned char temp = image_data[(y * input_batch->width + x) * input_batch->channels + c];
-                        new_data[((y * input_batch->width + x) * input_batch->channels + c) + i * image_size] =
-                            image_data[(y * input_batch->width + (input_batch->width - 1 - x)) * input_batch->channels + c];
-                        new_data[((y * input_batch->width + (input_batch->width - 1 - x)) * input_batch->channels + c) + i * image_size] = temp;
+                for (int c = 0; c < input_channels; ++c)
+                {
+                    if (y > flip_height)
+                    {
+                        output_image_data[((y * input_width + x) * input_channels + c)] =
+                            input_image_data[(y * input_width + x) * input_channels + c];
+                        output_image_data[((y * input_width + (input_width - 1 - x)) * input_channels + c)] =
+                            input_image_data[(y * input_width + (input_width - 1 - x)) * input_channels + c];
+                    }
+                    else
+                    {
+                        unsigned char temp = input_image_data[(y * input_width + x) * input_channels + c];
+                        output_image_data[((y * input_width + x) * input_channels + c)] =
+                            input_image_data[(y * input_width + (input_width - 1 - x)) * input_channels + c];
+                        output_image_data[((y * input_width + (input_width - 1 - x)) * input_channels + c)] = temp;
                     }
                 }
             }
         }
+
+        append_result_image(output_image_data, img_size);
     }
+}
 
-    finalize(input_batch, &mirrored_batch);
+ImageBatch run(ImageBatch *input_batch, ModuleParameterList *module_parameter_list)
+{
+    ImageBatch result_batch;
+    result = &result_batch;
+    input = input_batch;
+    config = module_parameter_list;
 
-    return mirrored_batch;
+    module();
+
+    finalize();
+
+    return result_batch;
 }
--- src/module_examples/module_gray.c
+++ src/module_examples/module_gray.c
@@ -1,42 +1,69 @@
 #include "module.h"
 #include "util.h"
 
-ImageBatch run(ImageBatch *input_batch, ModuleParameterList *config) {
-    // Create a new ImageBatch object to store the black and white image
-    ImageBatch gray_batch;
-    gray_batch.mtype = input_batch->mtype;
-    gray_batch.height = input_batch->height;
-    gray_batch.width = input_batch->width;
-    gray_batch.channels = 1; // Black and white image will have only one channel
-    gray_batch.num_images = input_batch->num_images;
+/* START MODULE IMPLEMENTATION */
+void module()
+{
+    /* Get metadata of input batch */
+    int width = get_input_width();
+    int height = get_input_height();
+    int channels = get_input_channels();
+    int num_images = get_input_num_images();
 
-    int batch_size = input_batch->width * input_batch->height * input_batch->channels * input_batch->num_images;
+    /* Set dimensions of output */
+    set_result_dimensions(width, height, 1);
+    set_result_num_images(num_images);
 
-    // Calculate the size of the black and white image data
-    size_t bwDataSize = gray_batch.height * gray_batch.width * gray_batch.channels * gray_batch.num_images;
-    unsigned char *new_data = (unsigned char *)malloc(bwDataSize);
-
-    if (new_data == NULL)
+    /* Example code for iterating a pixel value at a time */
+    for (int i = 0; i < num_images; ++i)
     {
-        fprintf(stderr, "[module_gray] Error: Unable to allocate memory.\n");
-        exit(EXIT_FAILURE);
+        unsigned char *input_image_data;
+        size_t input_image_size = get_image_data(i, &input_image_data);
+        size_t output_image_size = width * height;
+
+        /* Define temporary output image */
+        unsigned char *output_image_data = (unsigned char *)malloc(output_image_size);
+
+        if (output_image_data == NULL)
+        {
+            fprintf(stderr, "[module_template] Error: Unable to allocate memory.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; i < input_image_size; i += channels) 
+        {
+            // Extract color components from the original pixel
+            unsigned char r = input_image_data[i];
+            unsigned char g = input_image_data[i + 1];
+            unsigned char b = input_image_data[i + 2];
+
+            // Convert to grayscale using luminance method (0.299R + 0.587G + 0.114B)
+            unsigned char gray = (unsigned char)(0.299 * r + 0.587 * g + 0.114 * b);
+
+            output_image_data[i / channels] = gray;
+        }
+
+        /* Append the image to the result batch */
+        append_result_image(output_image_data, output_image_size);
+
+        /* Remember to free any allocated memory */
+        free(input_image_data);
+        free(output_image_data);
     }
+}
+/* END MODULE IMPLEMENTATION */
 
-    gray_batch.data = new_data;
+/* Main function of module (NO NEED TO MODIFY) */
+ImageBatch run(ImageBatch *input_batch, ModuleParameterList *module_parameter_list)
+{
+    ImageBatch result_batch;
+    result = &result_batch;
+    input = input_batch;
+    config = module_parameter_list;
 
-    for (int i = 0; i < batch_size; i += input_batch->channels) {
-        // Extract color components from the original pixel
-        unsigned char r = input_batch->data[i];
-        unsigned char g = input_batch->data[i + 1];
-        unsigned char b = input_batch->data[i + 2];
+    module();
 
-        // Convert to grayscale using luminance method (0.299R + 0.587G + 0.114B)
-        unsigned char gray = (unsigned char)(0.299 * r + 0.587 * g + 0.114 * b);
+    finalize();
 
-        new_data[i / input_batch->channels] = gray;
-    }
-
-    finalize(input_batch, &gray_batch);
-
-    return gray_batch;
+    return result_batch;
 }
